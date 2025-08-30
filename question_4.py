513165eaf815d7c2e4c33fb8d690b4012f1cfbf8
DhruvS <dhruvsdeepak@gmail.com>
2025-08-30 19:01:06 +0530

    added Q4

---
+++ question_4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import random as rd
+
+arr = []
+for i in range(5):
+    row = []
+    for j in range(5):
+        row.append(rd.randint(1, 100))
+    arr.append(row)
+arr = np.array(arr)
+
+newArr = arr[1:4, 1:4]
+print(newArr)
+
+lcol = arr[:, 4:5]
+frow = arr[0:1, :]
+
+print(np.dot(frow, lcol)[0][0])
