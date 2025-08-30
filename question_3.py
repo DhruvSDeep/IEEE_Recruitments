43775a0d06d32517dfb712fafde06161ad0892d6
DhruvS <dhruvsdeepak@gmail.com>
2025-08-30 18:49:18 +0530

    added Q3

---
+++ question_3.py
@@ -0,0 +1,21 @@
+import numpy as np
+import random as rd
+
+arr = []
+for i in range(5):
+    row = []
+    for j in range(5):
+        row.append(rd.randint(1, 100))
+    arr.append(row)
+arr = np.array(arr)
+
+max = max(arr.flatten())
+min = min(arr.flatten())
+mean = np.mean(arr)
+print("The maximum value in the array is:", max)
+print("The minimum value in the array is:", min)
+print("The mean value in the array is:", mean)
+
+arr = arr.flatten()
+arr.sort()
+print("The sorted array is:", arr)
