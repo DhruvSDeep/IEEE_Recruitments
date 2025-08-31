57bea97088a3a346224d3d2d440b09a97583b746
DhruvS <dhruvsdeepak@gmail.com>
2025-08-31 16:19:13 +0530

    comments added

--- question_2.py
+++ question_2.py
@@ -1,9 +1,11 @@
 para = input("Enter a paragraph: ").split()
-noneCheck = False
-for i in para:
-    if i == i[::-1]:
-        print(i)
-        noneCheck = True
 
-if noneCheck == False:
+noneCheck = False    #to check if no palindrome is found
+
+for i in para:
+    if i == i[::-1]:    #checking if the word is a palindrome
+        print(i)
+        noneCheck = True   #ensuring noneCheck is True when a palindrome is found
+
+if noneCheck == False:   #printing 0 if no palindrome is found
     print("0")
