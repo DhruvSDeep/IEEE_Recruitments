453aa45b8bb1c1faf64a757eb53b9408dfb22e29
DhruvS <dhruvsdeepak@gmail.com>
2025-08-31 16:19:23 +0530

    comments added

--- question_5.py
+++ question_5.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as mpl
 
-numsX = np.random.normal(loc=0, scale=1, size=1000)
+numsX = np.random.normal(loc=0, scale=1, size=1000)     #generating 1000 random numbers from normal distribution
 numsY = np.random.normal(loc=0, scale=1, size=1000)
 
-mpl.scatter(numsX, numsY)
+mpl.scatter(numsX, numsY)   #creating scatter plot, and labelling axes, and title
 mpl.xlabel('X-axis')
 mpl.ylabel('Y-axis')
 mpl.title('Scatter Plot of Randomly Generated Normally Chosen Numbers')
