01eaa575333755dd83b30bde8c394c5363e9da7d
DhruvS <dhruvsdeepak@gmail.com>
2025-08-31 15:45:05 +0530

    Changed to normalised form

--- question_4.py
+++ question_4.py
@@ -9,10 +9,17 @@ for i in range(5):
     arr.append(row)
 arr = np.array(arr)
 
-newArr = arr[1:4, 1:4]
+max = max(arr.flatten())     
+min = min(arr.flatten())
+mean = np.mean(arr)
+arr = ((arr - min) / (max - min))    # Till here is creating 5x5 normalised array
+
+
+
+newArr = arr[1:4, 1:4]    # extracting 3x3 array
 print(newArr)
 
 lcol = newArr[:, 2:3]
-frow = newArr[0:1, :]
+frow = newArr[0:1, :] 
 
-print(np.dot(frow, lcol)[0][0])
+print(np.dot(frow, lcol)[0][0])     # finding dot product of first row and last column of 3x3 array
