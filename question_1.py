0f148723d90ba2ddf17e782505a49e5ab620b94d
DhruvS <dhruvsdeepak@gmail.com>
2025-08-31 16:18:56 +0530

    comments added

--- question_1.py
+++ question_1.py
@@ -1,11 +1,11 @@
 n = int(input("enter value for n: "))
 
 for i in range (1, n+1):
-    for k in range(n-i):
+    for k in range(n-i):      #adding the spaces, equalling n-i
         print("  ", end="")
-    for j in range(i):
+    for j in range(i):        #adding the stars, equalling i
         print("*", end=" ")
-    print()
+    print()                   #to seperate rows into different lines
 
 
     
